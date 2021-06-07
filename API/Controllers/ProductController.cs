244f96e0eb9219e12b4d4f7588658c2903d75c7c
thoaianh <lengocthoaianh2000@gmail.com>
2021-06-07 22:07:05 +0700

    thoaianh api

---
+++ API/Controllers/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using API.Models;
+using DatabaseAccess;
+using System.Collections.Generic;
+using System.Web.Http;
+using RouteAttribute = System.Web.Http.RouteAttribute;
+
+
+namespace API.Controllers
+{
+    public class ProductController : ApiController
+    {
+        // GET: Product
+        IProduct prodRepos = new ProductRepo();
+        [Route("listProduct")]
+        [HttpGet]
+        public List<Product> GetListPro()
+        {
+            return prodRepos.listProduct();
+        }
+        [Route("listProduct/{id}")]
+        [HttpGet]
+        public Product GetPro(string id)
+        {
+            return prodRepos.findProduct(id);
+        }
+
+    }
+}
---
+++ API/Models/IProduct.cs
@@ -0,0 +1,15 @@
+﻿using DatabaseAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace API.Models
+{
+    interface IProduct
+    {
+        List<Product> listProduct();
+
+        Product findProduct(string id);
+    }
+}
