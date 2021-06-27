9c1e77802d7121f2921c7bba4d1a0dc2fbee25f9
huyền trang <nle995762@gmail.com>
2021-06-27 21:56:16 +0700

    trangDev2

---
+++ API/Controllers/ProductController.cs
@@ -0,0 +1,31 @@
+﻿using API.Models;
+using DatabaseAccess;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Web.Http;
+using RouteAttribute = System.Web.Http.RouteAttribute;
+
+
+namespace API.Controllers
+{
+    public class ProductController : ApiController
+    {
+        IProduct productRepos = new ProductRepo();
+        
+        [Route("listProduct")]
+        [HttpGet]
+        public List<Product> GetListProduct()
+        {
+            return productRepos.listProduct();
+        }
+       
+        [HttpGet]
+        public Product GetProduct(string id)
+        {
+            return productRepos.findProduct(id);
+        }
+       
+
+    }
+}
